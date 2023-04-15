83e3b0c19232d56776ff9205fd38b4a5caeaa6d7
Carl Xiong <xiongc05@gmail.com>
2023-04-15 09:12:36 +0000

    done ex1.3

--- ch1/echo3/main.go
+++ ch1/echo3/main.go
@@ -4,17 +4,48 @@
 // See page 8.
 
 // Echo3 prints its command-line arguments.
+// Review:
+// Here is an example of output:
+// $ go run main.go some   you know jij j llla jijiea a b c
+// some you know jij j llla jijiea a b c
+// echo1: 0.001706
+// some you know jij j llla jijiea a b c
+// echo3: 0.000240
+// 
+// After reading the section on benchmark, I found it would be not very useful to
+// benchmark a program printing a lot to the stdout.
 package main
 
 import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
+
+func echo1() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
+
+func echo3() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
 
 //!+
 func main() {
-	fmt.Println(strings.Join(os.Args[1:], " "))
+	// count := 100
+	beg_t := time.Now()
+	echo1()
+	fmt.Printf("echo1: %f\n", time.Since(beg_t).Seconds())
+
+	beg_t = time.Now()
+	echo3()
+	fmt.Printf("echo3: %f\n", time.Since(beg_t).Seconds())
 }
 
 //!-
