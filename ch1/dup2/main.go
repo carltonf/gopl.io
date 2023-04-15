735e51d79053656a383f6c6f6dd9cb607cdbd1c5
Carl Xiong <xiongc05@gmail.com>
2023-04-15 09:44:16 +0000

    fullfill the basic requirements

--- ch1/dup2/main.go
+++ ch1/dup2/main.go
@@ -16,9 +16,10 @@ import (
 
 func main() {
 	counts := make(map[string]int)
+	locations := make(map[string][]string)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, locations)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -26,21 +27,25 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, counts, locations)
 			f.Close()
 		}
 	}
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+			fmt.Printf("%d\t%s\t%s\n", n, line, locations[line])
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]int, locations  map[string][]string) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
+		loc_slice := locations[input.Text()]
+		if len(loc_slice) == 0 || loc_slice[len(loc_slice)-1] != f.Name() {
+			locations[input.Text()] = append(loc_slice, f.Name())
+		}
 	}
 	// NOTE: ignoring potential errors from input.Err()
 }
