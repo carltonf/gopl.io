82bc6f032068d4d181b66e2f8d4c86cf55347ab7
Carl Xiong <xiongc05@gmail.com>
2023-04-15 10:40:28 +0000

    Introduce struct line_info

--- ch1/dup2/main.go
+++ ch1/dup2/main.go
@@ -13,13 +13,16 @@ import (
 	"fmt"
 	"os"
 )
+type line_info struct {
+	count int
+	locations []string
+}
 
 func main() {
-	counts := make(map[string]int)
-	locations := make(map[string][]string)
+	counts := make(map[string]*line_info)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts, locations)
+		countLines(os.Stdin, counts)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -27,24 +30,31 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts, locations)
+			countLines(f, counts)
 			f.Close()
 		}
 	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\t%s\n", n, line, locations[line])
+	for line, info := range counts {
+		if info.count > 1 {
+			fmt.Printf("%d\t%s\t%s\n", info.count, line, info.locations)
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int, locations  map[string][]string) {
+func countLines(f *os.File, counts map[string]*line_info) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
-		counts[input.Text()]++
-		loc_slice := locations[input.Text()]
+		if counts[input.Text()] == nil {
+			counts[input.Text()] = &line_info{
+				count: 0,
+				locations: []string{},
+			}
+		}
+		counts[input.Text()].count++
+		loc_slice := counts[input.Text()].locations
 		if len(loc_slice) == 0 || loc_slice[len(loc_slice)-1] != f.Name() {
-			locations[input.Text()] = append(loc_slice, f.Name())
+			counts[input.Text()].locations = append(loc_slice, f.Name())
+
 		}
 	}
 	// NOTE: ignoring potential errors from input.Err()
